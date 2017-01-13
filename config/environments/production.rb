3742dbbea6ff4b086d17f4a3bd71f7a8ad676aef
shaolinxi <michal1chmiel@gmail.com>
2017-01-13 23:14:42 +0000

    working on configuration of s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,5 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
     :s3_host_name => "s3.eu-central-1.amazonaws.com", # Added entry
-    :url => ":s3_host_name"                        # Added entry
   }
 end
